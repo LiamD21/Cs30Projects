075af9769654a0182d7e9c0f8830fef2edc1685c
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2020-02-13 13:09:22 -0600

    fixed bounce, started the car drawing

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -9,24 +9,35 @@ let ballX = 800;
 let ballY = 300;
 let verticalV = 2;
 let horizontalV = 5;
+let carX = 300;
+let carY;
+let carWidth = 100;
+let carHeight = 60;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  carY = height - 75;
 }
 
 function draw() {
   background(220);
+
+  fill("red");
+  noStroke();
+  rect(carX, carY, carWidth, carHeight);
+  rect(carX - 40,carY + 20, 40, 40);
+
+  stroke("black");
   fill(245, 135, 66);
   ellipse(ballX, ballY, 75, 75);
   line(ballX, ballY + 75/2, ballX, ballY - 75/2);
-  noFill();
-  arc(ballX - 75/4, ballY - 75/4, 75, 75, 0, THIRD_PI)
 
-  if (ballY < height - 75/2.5){
+  if (ballY < height - 75/2){
     verticalV += 0.35;
   }
-  else if (ballY >= height - 75/2){
+  else if (ballY > height - 75/2){
     verticalV *= -0.75;
+    ballY = height - 75/2;
   }
 
   if (ballX > width - 75/2|| ballX < 75/2){
@@ -36,10 +47,17 @@ function draw() {
   if (horizontalV > 0){
     horizontalV -= 0.01;
   }
-  else if (horizontalV < -0){
+  else if (horizontalV < 0){
     horizontalV += 0.01;
   }
 
   ballY += verticalV;
   ballX += horizontalV;
+}
+
+function keyPressed(){
+  if (key === "m"){
+    ballX = 800;
+    ballY = 300;
+  }
 }
