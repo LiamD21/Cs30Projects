cf830c9e6ca7713ab200e8e1238f0ef4ab06c903
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2020-03-01 13:22:40 -0600

    start and end screens

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -16,10 +16,15 @@ let i;
 let lastBallSpawned = 0;
 let ballToRespawn;
 let lastNewBall = 0;
-let respawnTime = 1000;
+let respawnTime = 500;
+let startScreen = true;
+let playingGame = false;
+let gameOverScreen = false;
+let finalScore;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
   carY = height - 45;
 
   for (i = 0; i < 5; i++){
@@ -41,15 +46,43 @@ function setup() {
 
 function draw() {
   background(220);
+  if (startScreen){
+    drawStartScreen();
+  }
 
-  drawCar();
-  drawBall();
-  carMoveOnKeyPress();
-  writeTime();
-  ballBouncing();
-  respawnBalls();
-  detectHit();
-  addBalls();
+  if (playingGame){
+    drawCar();
+    drawBall();
+    carMoveOnKeyPress();
+    writeTime();
+    ballBouncing();
+    respawnBalls();
+    detectHit();
+    addBalls();
+  }
+
+  if (gameOverScreen){
+    drawGameOver();
+  }
+}
+
+function drawStartScreen(){
+  textSize(200);
+  fill(50, 50, 200);
+  text("Dropper", width/2 - 400, height/2)
+  fill("black");
+  textSize(50);
+  text("Press Space To Start", width/2  - 300, height/2 + 200);
+}
+
+function drawGameOver(){
+  textSize(200);
+  fill(200, 50, 50);
+  text("GAME OVER", width/2 - 600, height/2);
+  fill("black");
+  textSize(50);
+  text("You Survived For " + str(finalScore) + " Seconds!", width/2 - 350, height/2 + 100);
+  text("Press Space To Play Again", width/2 - 300, height/2 + 200);
 }
 
 function drawBall(){
@@ -88,10 +121,10 @@ function ballBouncing(){
     }
 
     if (ballArray[i].horizontalV > 0){
-      ballArray[i].horizontalV -= 0.01;
+      ballArray[i].horizontalV -= 0.005;
     }
     else if (ballArray[i].horizontalV < 0){
-      ballArray[i].horizontalV += 0.01;
+      ballArray[i].horizontalV += 0.005;
     }
 
     ballArray[i].ballY += ballArray[i].verticalV;
@@ -103,42 +136,48 @@ function detectHit(){
   for (i = 0; i < ballArray.length; i++){
     if (ballArray[i].ballX - 75/2 < carX + carWidth + 20 && ballArray[i].ballX + 75/2 > carX + 20){
       if (ballArray[i].ballY + 75/2 > carY){
-        text("LOSE", 500, 500);
+        finalScore = seconds;
+        playingGame = false;
+        gameOverScreen = true;
       }
     }
   }
 }
 
 function addBalls(){
-  if (millis() - lastNewBall > 5000){
-    let y = 100;
-    let vertV = random(1, 3);
-    let horV = random(-7, 7);
-    let x = random(75, width - 75);
+  if (playingGame){
+    if (millis() - lastNewBall > 3000){
+      let y = 100;
+      let vertV = random(1, 3);
+      let horV = random(-7, 7);
+      let x = random(75, width - 75);
 
-    let ball = {
-      ballX: x,
-      ballY: y,
-      verticalV: vertV,
-      horizontalV: horV
-    };
-    ballArray.push(ball);
+      let ball = {
+        ballX: x,
+        ballY: y,
+        verticalV: vertV,
+        horizontalV: horV
+      };
+      ballArray.push(ball);
 
-    lastNewBall = millis();
+      lastNewBall = millis();
+    }
   }
 }
 
 function respawnBalls(){
-  if (millis() - lastBallSpawned > respawnTime){
-    ballToRespawn = random(0, ballArray.length - 1);
-    ballToRespawn = Math.round(ballToRespawn);
+  if (playingGame){
+    if (millis() - lastBallSpawned > respawnTime){
+      ballToRespawn = random(0, ballArray.length - 1);
+      ballToRespawn = Math.round(ballToRespawn);
 
-    ballArray[ballToRespawn].ballY = 100;
-    ballArray[ballToRespawn].ballX = random(75, width - 75);
-    ballArray[ballToRespawn].verticalV = random(1, 3);
-    ballArray[ballToRespawn].horizontalV = random(-7, 7);
+      ballArray[ballToRespawn].ballY = 100;
+      ballArray[ballToRespawn].ballX = random(75, width - 75);
+      ballArray[ballToRespawn].verticalV = random(1, 3);
+      ballArray[ballToRespawn].horizontalV = random(-7, 7);
 
-    lastBallSpawned = millis();
+      lastBallSpawned = millis();
+    }
   }
 }
 
@@ -166,7 +205,7 @@ function carMoveOnKeyPress(){
 function writeTime(){
   textSize(50);
   fill(0);
-  seconds = millis()/1000;
+  seconds = (millis() - gameStartTime)/1000;
   seconds = seconds.toFixed(1);
   text(seconds, 100, 100);
 }
@@ -174,5 +213,14 @@ function writeTime(){
 function keyPressed(){
   if (key === "a" || key === "d"){
     carV = 1;
+  }
+  if (startScreen){
+    if (key === " "){
+      startScreen = false;
+      playingGame = true;
+      gameStartTime = millis();
+      lastNewBall = gameStartTime;
+      lastBallSpawned = gameStartTime;
+    }
   }
 }
