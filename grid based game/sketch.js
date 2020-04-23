a5f532b0b4121609e1857861c8501f2078ddc98d
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2020-04-23 13:07:09 -0600

    added selecting piece to move, fixed drawing pieces to make it simpler to make the game work

--- grid based game/sketch.js
+++ grid based game/sketch.js
@@ -13,14 +13,18 @@ let yOffset;
 let xOffset;
 let BEIGE = [207, 185, 157];
 let patternCount = 0;
-
+let selecting = false;
+let selectingI = 0;
+let selectingJ = 0;
+let selected = false;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
   strokeWeight(2);
   rectMode(CENTER);
+  noStroke();
 
-  cellSize = height / 12;
+  createCanvas(windowWidth, windowHeight);
+  cellSize = height / 10;
 
   yOffset = height/2 - (cellSize * (rows/2)) + cellSize/2;
   xOffset =  width/2 - (cellSize * (cols/2)) + cellSize/2;
@@ -31,6 +35,7 @@ function setup() {
       grid[i][j] = "empty";
     }
   }
+  setupGrid();
 }
 
 
@@ -38,6 +43,8 @@ function draw() {
   background(220);
   createGrid();
   addCheckers();
+  choosePiece();
+  movePiece();
 }
 
 function createGrid(){
@@ -58,20 +65,64 @@ function createGrid(){
   }
 }
 
-function addCheckers(){
+function setupGrid(){
   for (let i = 0; i < cols; i++){
     for (let j = 0; j < 2; j++){
-      fill("red");
-      ellipse(cellSize * i + xOffset, cellSize * j +yOffset, cellSize * 7/8, cellSize * 5/6);
       grid[i][j] = "enemy";
     }
   }
 
   for (let i = 0; i < cols; i++){
     for (let j = 6; j < 8; j++){
-      fill("blue");
-      ellipse(cellSize * i + xOffset, cellSize * j +yOffset, cellSize * 7/8, cellSize * 5/6);
       grid[i][j] = "player";
     }
+  }
+}
+
+function addCheckers(){
+  for (let i = 0; i < cols; i++){
+    for (let j = 0; j < rows; j++){
+      if(grid[i][j] === "enemy"){
+        fill("red");
+        ellipse(cellSize * i + xOffset, cellSize * j +yOffset, cellSize * 7/8, cellSize * 5/6);
+      }
+      else if (grid[i][j] === "player"){
+        fill("blue");
+        ellipse(cellSize * i + xOffset, cellSize * j +yOffset, cellSize * 7/8, cellSize * 5/6);
+      }
+    }
+  }
+}
+
+function choosePiece(){
+  for (let i = 0; i < cols; i++){
+    for (let j = 0; j < rows; j++){
+      if (collidePointRect(mouseX, mouseY, cellSize * i + xOffset - cellSize/2, cellSize * j + yOffset - cellSize/2, cellSize, cellSize)){
+        if (grid[i][j] === "player"){
+          strokeWeight(8);
+          stroke("yellow");
+          fill("blue")
+          selecting = true;
+          selectingI = i;
+          selectingJ = j;
+          ellipse(cellSize * i + xOffset, cellSize * j +yOffset, cellSize * 7/8, cellSize * 5/6);
+        }
+        else{
+          selecting = false;
+        }
+      }
+      noStroke();
+    }
+  }
+}
+
+function movePiece(){
+
+}
+
+function mouseClicked(){
+  if(selecting){
+    selected = true;
+    selecting = false;
   }
 }
