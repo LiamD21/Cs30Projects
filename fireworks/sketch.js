5cd0cd288c658dcfa694b144fcc59cb8b3f00dc5
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2020-05-15 18:09:09 -0600

    created fireworks start

---
+++ fireworks/sketch.js
@@ -0,0 +1,19 @@
+// Fireworks
+// Liam Dallaire
+// 
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+function createFireworks(){
+  
+}
