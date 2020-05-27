bbeb9be1259225bbfad3c9bb990b7fa77b4a7a28
LiamD21 <58191445+LiamD21@users.noreply.github.com>
2020-05-27 16:02:47 -0600

    addad birds

--- fireworks/sketch.js
+++ fireworks/sketch.js
@@ -6,20 +6,36 @@
 // the firework forms a circle, using trig to make each piece travel at a diffenent angle around the circle
 
 let fireworks = [];
+let birds = [];
+let up;
+let down;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  window.setInterval(addBird, 2000);
 }
 
 function draw() {
   background(20);
-  for (let i = 0; i < fireworks.length; i++){
-    fireworks[i].move();
-    fireworks[i].show();
-    if (fireworks[i].a <= 0){
-      fireworks.splice(i, 1);
+  for (let firework of fireworks){
+    firework.move();
+    firework.show();
+    if (firework.a <= 0){
+      fireworks.splice(firework, 1);
     }
   }
+
+  for (let bird of birds){
+    bird.update();
+    bird.display();
+    bird.isAlive();
+  }
+}
+
+function addBird(){
+  let y = random(0, height);
+  let bird = new Bird(0, y , "white");
+  birds.push(bird);
 }
 
 class Firework {
@@ -45,7 +61,29 @@ class Firework {
     noStroke();
     fill(this.r, this.g, this.b, this.a);
     ellipse(this.x, this.y, this.radius);
-    this.a -= 3;
+    this.a -= 2.8;
+  }
+}
+
+class Bird {
+  constructor(x, y, color, dy, up, down){
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
+  update(){
+    this.x += 3;
+  }
+
+  display(){
+    fill(this.color);
+    rect(this.x, this.y, 20, 10);
+  }
+
+  isAlive(){
+    if (this.x > width){
+      birds.splice(this, 1);
+    }
   }
 }
 
